54d89c4102912345be6a321d841487e052f7efcc
Venkata Raghu Nadakuditi <v-venad@microsoft.com>
2018-11-27 13:26:33 -0800

    Test WithConsole

---
+++ TestConsolde/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestConsolde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
